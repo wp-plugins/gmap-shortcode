243924c9deb018b3fdaf3836146ad4b0e00ace77
mcostales84 <mcostales84@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-20 03:25:45 +0000

    tagging v2.0. compatibility with new google maps
    git-svn-id: https://plugins.svn.wordpress.org/gmap-shortcode/trunk@1094887 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- gmap-shortcode.php
+++ gmap-shortcode.php
@@ -1,24 +1,38 @@
 <?php
 /**
  * Plugin Name: GMap Shortcode
- * Description: This plugin make you happy when you need to add Google Maps to your pages/posts. You only need to know the url of the map, and use the shortcode [gmap_mc src="google_map_url"]. 
- * Version: 1.0
+ * Description: This plugin make you happy when you need to add Google Maps to your pages/posts. You only need to know the latitude and the longitude, and use the shortcode [gmap_mc lat="LATITUDE" long="LONGITUDE"]. 
+ * Version: 2.0
  * Author: Manuel Costales
  * Author URI: http://www.manuelcostales.com
  */
 defined( 'ABSPATH' ) or die( 'Plugin file cannot be accessed directly.' );
 /*add javascripts to the header*/
-function gmap_shortcode_mc($atts, $content = null) {
-   extract(shortcode_atts(array(
-               "width" => '350',
-               "height" => '250',
-               "src" => ''
-   ), $atts));
- 
-return '<div>
-         <iframe src="'.$src.'&output=embed" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" width="'.$width.'" height="'.$height.'"></iframe>
-        </div>
-       ';
+function gmap_shortcode_mc_iframe( $atts )
+{
+$lat = $atts['lat'];
+$long = $atts['long'];
+$mapwidth = $atts['mapwidth'];
+$width = $atts['width'];
+$height = $atts['height'];
+
+if (! $lat) $lat = 21.5513258;
+if (! $long) $long = -79.6017351;
+if (! $mapwidth) $mapwidth = '1500km';
+if (! $width ) $width = 350;
+if (! $height ) $height = 250;
+
+preg_match_all('!\d+!', $mapwidth, $nums);
+$mapwidthnum = $nums[0][0];
+
+if ( strpos( $mapwidth, 'km' ) !== false )
+{$zoom = 3549 * $mapwidthnum;}
+else{$zoom = 5712 * $mapwidthnum;}
+
+$link = 'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d'.$zoom.'!2d'.$long.'!3d'.$lat.'!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1407089338193';
+$iframe = '<iframe src="'.$link.'" width="'.$width.'" height="'.$height.'" frameborder="0" style="border:0"></iframe>';
+
+return $iframe;
 }
- 
-add_shortcode("gmap_mc", "gmap_shortcode_mc");
+
+add_shortcode( 'gmap_mc', 'gmap_shortcode_mc_iframe' );
--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Donate Link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_i
 Tags: google map, 
 Requires at least: 3.0.1
 Tested up to: 4.1
-Stable tag: 1.0
+Stable tag: 2.0
 License: GPLv2
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -13,17 +13,21 @@ This plugin make you happy when you need to add Google Maps to your pages/posts.
 
 == Description ==
 
+WORKS WITH THE NEW VERSION OF GOOGLE MAPS
+
 You can add a map using a shortcode.
 
 You only need to:
 1. Go to https://www.google.com/maps
 2. Search the address you want to embed in your website. Drag and drop the map to adjust it to the section you want.
-3. Copy the url.
-4. Go to the page you want to add the map and paste this shortcode: [gmap_mc src="google_map_url"]
+3. Copy the latitude and longitude.
+4. Go to the page you want to add the map and paste this shortcode: [gmap_mc lat="LATITUDE" long="LONGITUDE"]
 
 * To keep in mind:
 - The default dimensions are 350x250
-- You can set your own dimensions using these parameters [gmap_mc width="600" height="250" src="google_map_url"]
+- The defaults latitude and longitude point to Cuba
+- You can set your own dimensions using these parameters [gmap_mc lat="LATITUDE" long="LONGITUDE" width="600" height="250"]
+- You can set the zoom of your map using the parameter mapwidth, like this [gmap_mc lat="LATITUDE" long="LONGITUDE" width="600" height="250" mapwidth="3000km"]. The zoom is assumed to be in miles unless you specify km. The default zoom is 1500km
 
 == Installation ==
 
@@ -41,6 +45,9 @@ or
 Send me an email at mcostales@jumptoweb.com and I will answer you as soon as I can.
 
 == Changelog ==
+
+= 2.0 =
+Add compatibility with new version of Google Map.
 
 = 1.0 =
 Just launch the plugin!
